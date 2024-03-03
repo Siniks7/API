667a82dfa40a40ea290eb99f25e3fa923ec66705
Nikolay Sponarovskiy <siniks7@yandex.ru>
2024-03-03 12:31:05 +0700

    улучшение DI

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,13 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import { injectable, inject } from 'inversify';
 import { BaseController } from '../common/base.controller';
 import { HTTPError } from '../errors/http-error.class';
-import { LoggerService } from '../logger/logger.service';
+import { IUserController } from './users.controller.interface';
 import { ILogger } from '../logger/logger.interface';
 import { TYPES } from '../types';
 import 'reflect-metadata';
 
 @injectable()
-export class UserController extends BaseController {
+export class UserController extends BaseController implements IUserController {
     constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
         super(loggerService);
         this.bindRoutes([
