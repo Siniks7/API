6d69f0e6cacf142ae07bc38ac4821053726ea98d
Nikolay Sponarovskiy <siniks7@yandex.ru>
2024-03-03 12:26:56 +0700

    inversify

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,6 +1,10 @@
 import { Logger, ILogObj } from 'tslog';
 import { ILogger } from './logger.interface';
+import 'reflect-metadata';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
 
+@injectable()
 export class LoggerService implements ILogger {
     public logger: Logger<ILogObj>;
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
+import { injectable, inject } from 'inversify';
 import { BaseController } from '../common/base.controller';
-import { LoggerService } from '../logger/logger.service';
 import { HTTPError } from '../errors/http-error.class';
+import { LoggerService } from '../logger/logger.service';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+import 'reflect-metadata';
 
+@injectable()
 export class UserController extends BaseController {
-    constructor(logger: LoggerService) {
-        super(logger);
+    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+        super(loggerService);
         this.bindRoutes([
             { path: '/login', method: 'post', func: this.login },
             { path: '/register', method: 'post', func: this.register }
