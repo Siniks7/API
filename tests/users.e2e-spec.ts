2f9c585ff402de9612c13972d01cb9541607a09d
Nikolay Sponarovskiy <siniks7@yandex.ru>
2024-03-10 20:47:10 +0700

    finish!

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -16,6 +16,36 @@ describe('Users e2e', () => {
 			.send({ email: 'a@aaa.ru', password: '12',  name: 'Aa' });
 		expect(res.statusCode).toBe(422);
 	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'siniks7@yandex.ru', password: '123456' });
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.ru', password: '1' });
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'siniks7@yandex.ru', password: '123456' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+		expect(res.body.email).toBe('siniks7@yandex.ru');
+	});
+
+	it('Info - error', async () => {
+		const res = await request(application.app).get('/users/info').set('Authorization', 'Bearer 1');
+		expect(res.statusCode).toBe(401);
+	});
+
 });
 
 afterAll(() => {
